a000e7a1253d6a53aa81e64d3ff6f76a3bcc416d
p-obrthr <pascal.oberthuer@web.de>
2023-09-09 16:09:28 +0200

    sudoku square set up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <table>
+          <tbody>
+            {
+              [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rIndex) =>  {
+                return <tr key={rIndex}>
+                  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
+                    return <td key={rIndex + cIndex}>
+                      <input className = "cellInput"/>
+                    </td>
+                  })}
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
       </header>
     </div>
   );
