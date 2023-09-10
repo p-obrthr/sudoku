b3386702d2f8664901369fb9e6e2cf191692a8cb
p-obrthr <pascal.oberthuer@web.de>
2023-09-10 18:14:19 +0200

    create header, buttons, solve function

--- src/App.js
+++ src/App.js
@@ -28,16 +28,74 @@ function App() {
 		}
 	setSudokuArr(grid);
   	}
+
+	function solve() {
+		if (solveSudoku(0, 0)) {
+		  	setSudokuArr([...sudokuArr]);
+		} else {
+		  	alert("no solution");
+		}
+	}
+	  
+	function solveSudoku(row, col) {
+		if (row === 9) {
+			return true;
+		}
+	  
+	if (sudokuArr[row][col] !== -1) {
+			return col === 8 ? solveSudoku(row + 1, 0) : solveSudoku(row, col + 1);
+	}
+	  
+	for (let num = 1; num <= 9; num++) {
+		if (isValidMove(row, col, num)) {
+			sudokuArr[row][col] = num;
+	  
+			if (col === 8 ? solveSudoku(row + 1, 0) : solveSudoku(row, col + 1)) {
+			  return true;
+			}
+	  
+			sudokuArr[row][col] = -1;
+		  }
+		}
+	  
+		return false;
+	  }
+	  
+	function isValidMove(row, col, num) {
+		for (let i = 0; i < 9; i++) {
+		  if (sudokuArr[row][i] === num || sudokuArr[i][col] === num) {
+			return false;
+		  }
+		}
+	  
+		const startRow = Math.floor(row / 3) * 3;
+		const startCol = Math.floor(col / 3) * 3;
+		for (let i = startRow; i < startRow + 3; i++) {
+		  for (let j = startCol; j < startCol + 3; j++) {
+			if (sudokuArr[i][j] === num) {
+			  return false;
+			}
+		  }
+		}
+	  
+		return true;
+	  }
+
+	function reset() {
+		setSudokuArr(getDeepCopy(matrix));
+	}
+	  
 return (
 	<div className="App">
-	  <header className="App-header">
+	  <div className="App-header">
+		<h3>let's solve your sudoku:</h3>
 		<table>
 		  <tbody>
 			{
 			  [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rIndex) =>  {
-				return <tr key={rIndex}>
+				return <tr key={rIndex} className={(row + 1) %3 === 0 ? 'bBorder' : ''}>
 				  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
-					return <td key={rIndex + cIndex}>
+					return <td key={rIndex + cIndex} className={(col + 1) %3 === 0 ? 'rBorder' : ''}>
 					  <input onChange={(e) => onInputChange(e, row, col)} 
 					  value={sudokuArr[row][col] === -1 ? '' : sudokuArr[row][col]} 
 					  className = "cellInput"
@@ -50,7 +108,11 @@ return (
 			}
 		  </tbody>
 		</table>
-	  </header>
+		<div className="buttonContainer">
+			<button className="button-30" onClick={solve}>solve</button>
+			<button className="button-30" onClick={reset}>reset</button>
+	  </div>
+	  </div>
 	</div>
   );
 }
