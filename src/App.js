d980adf33a52477202400d843df5aa8fa6fafa4e
p-obrthr <pascal.oberthuer@web.de>
2023-09-12 00:00:32 +0200

    fix bugs

--- src/App.js
+++ src/App.js
@@ -114,6 +114,9 @@ function App() {
 	  }
 
 	function solve() {
+		if (solved) {
+			return;
+		}
 		const inputs = document.querySelectorAll(".cellInput");
   		inputs.forEach((input) => {
 		const row = parseInt(input.getAttribute("data-row"));
@@ -126,6 +129,7 @@ function App() {
 			setSolved(true);
 		} else {
 		  	alert("no solution");
+			reset();
 		}
 	} 
 	  
@@ -185,14 +189,14 @@ function App() {
 return (
 	<div className="App">
 	  <div className="App-header">
-		<h3>sudoku:</h3>
+		<h3>sudoku</h3>
 		<table>
 		  <tbody>
 			{
 			  [0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rIndex) =>  {
-				return <tr key={rIndex} className={(row + 1) %3 === 0 ? 'bBorder' : ''}>
+				return <tr key={rIndex} className={(row + 1) %3 === 0 && (row + 1) !== 9 ? 'bBorder' : ''}>
 				  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
-					return <td key={rIndex + cIndex} className={(col + 1) %3 === 0 ? 'rBorder' : ''}>
+					return <td key={rIndex + cIndex} className={(col + 1) %3 === 0 && (col + 1) !== 9 ? 'rBorder' : ''}>
 					  <input 
 					  onChange={(e) => onInputChange(e, row, col)} 
 					  value={sudokuArr[row][col] === -1 ? '' : sudokuArr[row][col]} 
