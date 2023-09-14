aa8f04778d091767d13b9c33f7aa5084786544d5
p-obrthr <pascal.oberthuer@web.de>
2023-09-14 18:16:00 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-
-	const defaultMatrix = [ 
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1],
-		[-1, -1, -1, -1, -1, -1, -1, -1, -1]
-	];
+	const defaultMatrix = Array.from({ length: 9 }, () => Array(9).fill(-1));
 
 	const [sudokuArr, setSudokuArr] = useState(getDeepCopy(defaultMatrix));
 	const [solved, setSolved] = useState(false);
@@ -28,147 +17,81 @@ function App() {
 
 	function fillRandomMatrix() {
 		let randomMatrix = getDeepCopy(defaultMatrix);
+		const matrixSize = 3;
+		let x = 0;
 
-		let a = [1,2,3,4,5,6,7,8,9];
-		shuffleArray(a);
-
-		for(let row=0; row<3; row++) {
-			for(let col=0; col<3; col++) {
-
-				randomMatrix[row][col] = a[0];
-				a.shift();
-
+		for(let r=0; r<2; r++) {
+			let numbers = shuffleArray([1,2,3,4,5,6,7,8,9]);
+			for(let row=x; row <matrixSize; row++) {
+				for(let col=x; col <matrixSize; col++) {
+					randomMatrix[row][col] = numbers.shift();
+				}
 			}
+			x += matrixSize;
 		}
+		randomMatrix = deleteNumbers(matrixSolve(randomMatrix), 40); 
 
-		a = [1,2,3,4,5,6,7,8,9];
-		shuffleArray(a);
-
-		for(let row=3; row<6; row++) {
-			for(let col=3; col<6; col++) {
-				
-				randomMatrix[row][col] = a[0];
-				a.shift();
-
-
-			}
-		}
-
-		a = [1,2,3,4,5,6,7,8,9];
-		shuffleArray(a);
-
-		for(let row=6; row<9; row++) {
-			for(let col=6; col<9; col++) {
-				randomMatrix[row][col] = a[0];
-				a.shift();
-
-			}
-		}
-			
-		function matrixSolve(matrix) {
-			function isSafe(row, col, num) {
-			  for (let i = 0; i < 9; i++) {
-				if (matrix[row][i] === num || matrix[i][col] === num) {
-				  return false;
-				}
-			  }
-			  
-			  const startRow = Math.floor(row / 3) * 3;
-			  const startCol = Math.floor(col / 3) * 3;
-			  
-			  for (let i = 0; i < 3; i++) {
-				for (let j = 0; j < 3; j++) {
-				  if (matrix[startRow + i][startCol + j] === num) {
-					return false;
-				  }
-				}
-			  }
-			  
-			  return true;
-			}
-		  
-			function back() {
-			  for (let row = 0; row < 9; row++) {
-				for (let col = 0; col < 9; col++) {
-				  if (matrix[row][col] === -1) {
-					for (let num = 1; num <= 9; num++) {
-					  if (isSafe(row, col, num)) {
-						matrix[row][col] = num;
-						if (back()) {
-						  return true;
-						}
-						matrix[row][col] = -1;
-					  }
-					}
-					return false;
-				  }
-				}
-			  }
-			  return true; 
-			}
-		
-			if (back()) {
-			  return matrix;
-			} 
-		  }
-		  
-		  const solution = matrixSolve(randomMatrix);
-
-		  for(let i=0; i<10; i++) {
-
-			while(true) {
-				let row = Math.floor(Math.random() * 9);
-				let col = Math.floor(Math.random() * 9);
-				if (solution[row][col] !== -1) {
-					solution[row][col] = -1;
-					break;
-				}
-			}
-		  }
-
-
-		setSudokuArr(solution);
-   
+		setSudokuArr(randomMatrix);
 	}
 
 	function shuffleArray(array) {
 		for (let i = array.length - 1; i > 0; i--) {
-		  const j = Math.floor(Math.random() * (i + 1));
-		  [array[i], array[j]] = [array[j], array[i]];
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
 		}
-	  }
-
-	function isValidInRow(board, row, num) {
-		return !board[row].includes(num);
-	  }
-
-	function isValidInCol(board, col, num) {
-		for (let i = 0; i < 9; i++) {
-		  if (board[i][col] === num) {
-			return false;
-		  }
-		}
-		return true;
-	  }
-
-	function isValidInBox(board, row, col, num) {
-		const boxStartRow = Math.floor(row / 3) * 3;
-		const boxStartCol = Math.floor(col / 3) * 3;
-		for (let i = boxStartRow; i < boxStartRow + 3; i++) {
-		  for (let j = boxStartCol; j < boxStartCol + 3; j++) {
-			if (board[i][j] === num) {
-			  return false;
+		return array;
+	}
+			
+	function matrixSolve(matrix) {
+		  
+		function back() {
+			for (let row = 0; row < 9; row++) {
+				for (let col = 0; col < 9; col++) {
+					if (matrix[row][col] === -1) {
+						for (let num = 1; num <= 9; num++) {
+					  		if (isValidMove(matrix, row, col, num)) {
+								matrix[row][col] = num;
+								if (back()) {
+						  			return true;
+								}
+								matrix[row][col] = -1;
+					  		}
+						}
+						return false;
+				  	}
+				}
 			}
-		  }
+			return true; 
 		}
-		return true;
-	  }
+		
+		if (back()) {
+			return matrix;
+		} 
+		  
+	}
+		  
+	function deleteNumbers (sudoku, count) {
+		for(let i=0; i<count; i++) {
+
+			while(true) {
+				let row = Math.floor(Math.random() * 9);
+				let col = Math.floor(Math.random() * 9);
+				if (sudoku[row][col] !== -1) {
+					sudoku[row][col] = -1;
+					break;
+				}
+			}
+		}
+		return sudoku;
+	}
+   
+	
 
   	function onInputChange(e, row, col) {
 		if (!solved) {
 		  const val = parseInt(e.target.value) || -1;
 		  if (val === -1 || (val >= 1 && val <= 9)) {
-			const updatedSudoku = getDeepCopy(sudokuArr);
+			let updatedSudoku = getDeepCopy(sudokuArr);
 			updatedSudoku[row][col] = val;
 			setSudokuArr(updatedSudoku);
 		  }
@@ -181,18 +104,15 @@ function App() {
 		}
 		const inputs = document.querySelectorAll(".cellInput");
   		inputs.forEach((input) => {
-		const row = parseInt(input.getAttribute("data-row"));
-		const col = parseInt(input.getAttribute("data-col"));
-		if (sudokuArr[row][col] !== -1) {
-			input.setAttribute("disabled", "true");
-		}
-	});
+			const row = parseInt(input.getAttribute("data-row"));
+			const col = parseInt(input.getAttribute("data-col"));
+			if (sudokuArr[row][col] !== -1) {
+				input.setAttribute("disabled", "true");
+			}
+		});
 		if (solveSudoku(0, 0)) {
 			setSolved(true);
-		} else {
-		  	alert("no solution");
-			reset();
-		}
+		} 
 	} 
 	  
 	function solveSudoku(row, col) {
@@ -200,28 +120,26 @@ function App() {
 			return true;
 		}
 	  
-	if (sudokuArr[row][col] !== -1) {
+		if (sudokuArr[row][col] !== -1) {
 			return col === 8 ? solveSudoku(row + 1, 0) : solveSudoku(row, col + 1);
-	}
-	  
-	for (let num = 1; num <= 9; num++) {
-		if (isValidMove(row, col, num)) {
-			sudokuArr[row][col] = num;
-	  
-			if (col === 8 ? solveSudoku(row + 1, 0) : solveSudoku(row, col + 1)) {
-			  return true;
-			}
-	  
-			sudokuArr[row][col] = -1;
-		  }
 		}
 	  
+		for (let num = 1; num <= 9; num++) {
+			if (isValidMove(sudokuArr, row, col, num)) {
+				sudokuArr[row][col] = num;
+				if (col === 8 ? solveSudoku(row + 1, 0) : solveSudoku(row, col + 1)) {
+			  		return true;
+				}
+	  			sudokuArr[row][col] = -1;
+		 	}
+		}
 		return false;
-	  }
+	}
 	  
-	function isValidMove(row, col, num) {
+	function isValidMove(matrix, row, col, num) {
+
 		for (let i = 0; i < 9; i++) {
-		  if (sudokuArr[row][i] === num || sudokuArr[i][col] === num) {
+		  if (matrix[row][i] === num || matrix[i][col] === num) {
 			return false;
 		  }
 		}
@@ -230,12 +148,11 @@ function App() {
 		const startCol = Math.floor(col / 3) * 3;
 		for (let i = startRow; i < startRow + 3; i++) {
 		  for (let j = startCol; j < startCol + 3; j++) {
-			if (sudokuArr[i][j] === num) {
+			if (matrix[i][j] === num) {
 			  return false;
 			}
 		  }
 		}
-	  
 		return true;
 	  }
 
@@ -282,6 +199,7 @@ return (
 	  </div>
 	</div>
   );
+
 }
 
 export default App;
